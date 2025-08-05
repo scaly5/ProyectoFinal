2c216db1151ca9b10fd874d809b1f6e5fa186753
scaly5 <videojuegosliceo@gmail.com>
2025-08-05 09:26:39 +0100

    martes.avance.1

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneM: MonoBehaviour
+{
+    public void play()
+{
+    SceneManager.LoadScene("nivel 1", LoadSceneMode.Single);
+    
+}
+
+    public void menu()
+{
+    SceneManager.LoadScene("MENU", LoadSceneMode.Single);
+}
+    public void nivel2()
+{
+    SceneManager.LoadScene("NIVEL 2", LoadSceneMode.Single);
+}
+    public void creditos()
+{
+    SceneManager.LoadScene("CREDITOS", LoadSceneMode.Single);
+}
+}
