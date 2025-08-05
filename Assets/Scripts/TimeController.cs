e83523745ce1b83bfba5091d16a1d685f25208c0
scaly5 <videojuegosliceo@gmail.com>
2025-08-05 13:50:11 +0100

    nivel1

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;//llamamos a su biblioteca
 
 public class TimeController : MonoBehaviour
 {
     public GameObject menuPausa;
     public float scaleAtRuntime;
-
     public void HandleTime()
     {
         if (Time.timeScale != 0)
@@ -19,21 +17,16 @@ public class TimeController : MonoBehaviour
         {
             Resume();
         }
-
     }
     public void Pause()
     {
         scaleAtRuntime = Time.timeScale;
         Time.timeScale = 0;
         menuPausa.SetActive(true);
-        EventSystem.current.SetSelectedGameObject(null); //deselecciona el boton aunque le hayamos hecho click
     }
     public void Resume()
     {
         Time.timeScale = scaleAtRuntime;
         menuPausa.SetActive(false);
-        EventSystem.current.SetSelectedGameObject(null);//deselecciona el boton aunque le hayamos hecho click
     }
-
 }
-
