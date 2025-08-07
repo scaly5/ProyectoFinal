04e3e8980b8bca26b2778b6cece994eed730a408
scaly5 <videojuegosliceo@gmail.com>
2025-08-07 11:29:16 +0100

    ladosPlataforma

---
+++ Assets/Scripts/PaLosLados.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaLosLados : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public float plataformSpeed = 2;
+    private int waypointsIndex = 0;
+    private int direction = 1; 
+
+    void Update()
+    {
+        MovePlataform();
+    }
+
+    void MovePlataform()
+    {
+        if (Vector3.Distance(transform.position, waypoints[waypointsIndex].transform.position) < 0.1f)
+        {
+            waypointsIndex += direction;
+
+            
+            if (waypointsIndex >= waypoints.Length)
+            {
+                waypointsIndex = waypoints.Length - 2;
+                direction = -1;
+            }
+            else if (waypointsIndex < 0)
+            {
+                waypointsIndex = 1;
+                direction = 1;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            waypoints[waypointsIndex].transform.position,
+            plataformSpeed * Time.deltaTime
+        );
+    }
+}
