ba4b9e739b3030d3e9245497e7f7d7bb2e216c45
scaly5 <videojuegosliceo@gmail.com>
2025-08-08 10:56:33 +0100

    retoques.viernes

--- Assets/Scripts/win2.cs
+++ Assets/Scripts/win2.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+
 public class Win2 : MonoBehaviour
 {
     public GameObject win2Text; //panel nuevo
- public void OnTriggerEnter2D(Collider2D collider){
-    Debug.Log("se tocan ");
+ public void OnTriggerEnter2D(Collider2D collider)
+ {
+    Debug.Log("se tocan");
     
     if(collider.gameObject.CompareTag("Player")){
     win2Text.SetActive(true);
